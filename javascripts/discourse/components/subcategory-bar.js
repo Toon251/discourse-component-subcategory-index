6878597e3287c2ec5db43114e1f3882f7795e34b
Toon251 <toon251@gmail.com>
2025-04-16 07:03:54 +0000

    use == for is current page

--- javascripts/discourse/components/subcategory-bar.js
+++ javascripts/discourse/components/subcategory-bar.js
@@ -55,7 +55,9 @@ export default class SubscriptionBar extends Component {
       return /Mobi|Android/i.test(navigator.userAgent);
     }
 
-
+    isCurrentPage (p) {
+      return this.currentPage == p
+    }
     
     isLetterFilter(s, f) {
       const r = f === "" || s.toUpperCase().charAt(0) === f;
