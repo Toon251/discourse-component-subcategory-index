adeed61c11d9a546c9dffb9794e49155a81f3c86
Toon251 <toon251@gmail.com>
2025-04-16 07:13:15 +0000

    Debug

--- javascripts/discourse/components/subcategory-bar.js
+++ javascripts/discourse/components/subcategory-bar.js
@@ -55,8 +55,8 @@ export default class SubscriptionBar extends Component {
       return /Mobi|Android/i.test(navigator.userAgent);
     }
 
-    isCurrentPage (p) {
-      return 1 == p
+    isCurrentPage (cp, p) {
+      return  cp == p
     }
     
     isLetterFilter(s, f) {
